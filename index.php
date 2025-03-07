c12fc3b769cb2b719591160eab481f264ad5759c
Leonardo Dinelli <leodinelli2007@gmail.com>
2025-03-07 19:32:50 -0300

    section blue ready
    the texts and icons in the section blue are ready

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
 </head>
 <body>
     <header> 
@@ -14,12 +15,50 @@
         </div>
 
         <div class="iconInstagram">
-        <a href="https://www.instagram.com/besouro_martelinho/" class="fa fa-instagram"></a>
+        <a href="https://www.instagram.com/besouro_martelinho/" class="fa-brands fa-instagram"></a>  
         </div>
 
         <div class="iconInstagram">
-        <a href="https://api.whatsapp.com/send?phone=5511947645783&text=Oi! Vi o anúncio e quero saber mais" class="fa fa-whatsapp"></a>
+        <a href="https://api.whatsapp.com/send?phone=5511947645783&text=Oi! Vi o anúncio e quero saber mais" class="fa-brands fa-whatsapp"></a>
         </div>
     </header>
+
+        <div class="section blue">
+            <p>Há mais de 25 anos <br>
+                trazendo soluções <br>
+                de alta qualidade <br>
+                <b>reparos automotivos</b> <br><br>
+            </p>
+                
+            <div class="iconsSectionBlue">
+                <div class="iconText"> 
+                    <i class="fa-solid fa-location-dot"></i> 
+                    <p>Av. Imirim, 2676 - Imirim - São Paulo/SP</p>
+                </div>
+
+                <div class="iconText">
+                    <i class="fa-solid fa-xmark"></i>
+                    <p>Segunda - Sexta, 8h ás 18h</p>
+                </div>
+
+                <div class="iconText">
+                    <i class="fa-solid fa-phone"></i>
+                    <p>Telefone: (11) 2239-8008</p>
+                </div>
+
+                <div class="iconText">
+                    <i class="fa-brands fa-whatsapp"></i>
+                    <p>Whatsapp: (11) 94764-5783</p>
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="section yellow">
+
+        </div>
+
+
 </body>
 </html>
