c7ef76feffbb3d815aae6c831a8e95b2b9559b4b
Leonardo Dinelli <leodinelli2007@gmail.com>
2025-03-07 15:17:44 -0300

    header ready
    the header and your buttons are ready

--- index.php
+++ index.php
@@ -4,8 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-    
+    <header> 
+        <div class="logoSite">
+            <img src="assets/images/logo.png" alt=""> 
+        </div>
+
+        <div class="iconInstagram">
+        <a href="https://www.instagram.com/besouro_martelinho/" class="fa fa-instagram"></a>
+        </div>
+
+        <div class="iconInstagram">
+        <a href="https://api.whatsapp.com/send?phone=5511947645783&text=Oi! Vi o anúncio e quero saber mais" class="fa fa-whatsapp"></a>
+        </div>
+    </header>
 </body>
 </html>
