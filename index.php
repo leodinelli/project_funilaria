b79c86d529b32b29770e2b72bd34621406f2ef21
Leonardo Dinelli <leodinelli2007@gmail.com>
2025-03-12 16:00:39 -0300

    finish the other sections
    section, descreption, reviews amd location are ready.

--- index.php
+++ index.php
@@ -23,39 +23,53 @@
         </div>
     </header>
 
-        <div class="section blue">
-            <p>Há mais de 25 anos <br>
-                trazendo soluções <br>
-                de alta qualidade <br>
-                <b>reparos automotivos</b> <br><br>
-            </p>
-                
-            <div class="iconsSectionBlue">
-                <div class="iconTextBlue"> 
-                    <i class="fa-solid fa-location-dot"></i> 
-                    <p>Av. Imirim, 2676 - Imirim - São Paulo/SP</p>
-                </div>
+    <div class="section blue">
+    <div class="content">
+        <p>Há mais de 25 anos <br>
+            trazendo soluções <br>
+            de alta qualidade <br>
+            <b>reparos automotivos</b> <br><br>
+        </p>
 
-                <div class="iconTextBlue">
-                    <i class="fa-solid fa-xmark"></i>
-                    <p>Segunda - Sexta, 8h ás 18h</p>
-                </div>
-
-                <div class="iconTextBlue">
-                    <i class="fa-solid fa-phone"></i>
-                    <p>Telefone: (11) 2239-8008</p>
-                </div>
-
-                <div class="iconTextBlue">
-                    <i class="fa-brands fa-whatsapp"></i>
-                    <p>Whatsapp: (11) 94764-5783</p>
-                </div>
+        <div class="iconsSectionBlue">
+            <div class="iconTextBlue"> 
+                <i class="fa-solid fa-location-dot"></i> 
+                <p>Av. Imirim, 2676 - Imirim - São Paulo/SP</p>
             </div>
 
+            <div class="iconTextBlue">
+                <i class="fa-solid fa-xmark"></i>
+                <p>Segunda - Sexta, 8h às 18h</p>
+            </div>
 
+            <div class="iconTextBlue">
+                <i class="fa-solid fa-phone"></i>
+                <p>Telefone: (11) 2239-8008</p>
+            </div>
+
+            <div class="iconTextBlue">
+                <i class="fa-brands fa-whatsapp"></i>
+                <p>Whatsapp: (11) 94764-5783</p>
+            </div>
         </div>
 
-        <div class="section yellow">
+        <div class="tellButton">
+            <br>
+            <a href="https://api.whatsapp.com/send?phone=5511947645783&text=Oi! Vi o anúncio e quero saber mais" 
+            class="fa-brands fa-whatsapp">­­ Fale conosco!</a>
+        </div>
+    </div>
+
+   
+    <div class="phone-mockup">
+        <div class="screen">
+            <video src="assets/images/video.mp4" controls loop muted></video>
+        </div>
+    </div>
+</div>
+
+
+ <div class="section yellow">
 
             <div class="icons-container">
 
@@ -97,6 +111,54 @@
         
     </div>
 </div>
+
+<div class="section descreption">
+    <div class="besouro">
+        <img src="assets/images/besouro.png" alt=""> 
+    </div>
+
+    <div class="text">
+        <p>A Besouro Martelinho de Ouro está no mercado de reparos automotivos há <br>
+            mais de 25 anos, oferecendo o que há de melhor em funilaria, pintura, <br>
+            martelinho de ouro e tudo o que seu carro precisa para ficar sempre em dia. <br><br>
+
+            Nossos profissionais são altamente qualificados para entregar o resultado que <br>
+            você merece: o melhor! <br> <br>
+
+            Fale com a gente, venha fazer uma avaliação em nossa oficina e <br>
+            #VemPraBesouro! <br><br>
+        </p>
+
+        <div class="tellButton">
+            <a href="https://api.whatsapp.com/send?phone=5511947645783&text=Oi! Vi o anúncio e quero saber mais" 
+            class="fa-brands fa-whatsapp">­­ Fale conosco!</a>
+        </div>
+    </div>
+</div>
+
+<div class="darkBlue">
+    <h2>Acompanhe nosso Instagram</h2>
+</div>
+
+<div class="section images">
+    <h2>Depoimento do Clientes</h2>
+    <img src="assets/images/img4.webp" alt="">
+    <img src="assets/images/img6.webp" alt="">
+    <img src="assets/images/img1.webp" alt="">
+    <img src="assets/images/img1.webp" alt="">
+    <img src="assets/images/img3.webp" alt="">
+    <img src="assets/images/img7.webp" alt="">
+</div>
+
+<div class="section location">
+    <h2>#VemPraBesouro</h2>
+    <br>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3659.361213367385!2d-46.65611792467225!3d-23.48349557885335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cef798e4fc0039%3A0xf04ea39207e196bc!2sBesouro%20Martelinho%20De%20Ouro!5e0!3m2!1spt-BR!2sbr!4v1741805201336!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
+
+<footer>
+        <p>Todos os direitos reservados – Besouro Martelinho de Ouro © 2023</p>
+</footer>
 
 
 </body>
